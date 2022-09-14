fa49fe2fa955a0ea45291426397f21f376a4542e
AIE\s200593 <s200593@students.aie.edu.au>
2022-09-14 14:25:28 +0930

    Added audio and shooting sound randomiser

---
+++ Team4MusicGameJam-Cat-(Game)/Assets/Scripts/randomSoundGenerator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomSoundGenerator : MonoBehaviour
+{
+    public AudioClip[] sounds;
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            source.clip = sounds[Random.Range(0, sounds.Length)];
+            source.Play();
+        }
+    }
+}
